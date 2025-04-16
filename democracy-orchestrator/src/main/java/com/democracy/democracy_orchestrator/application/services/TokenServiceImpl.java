5e0db34bd57acb82943c90cce010121f808749e3
raul <raraherher9274@gmail.com>
2025-04-16 11:13:04 -0500

    Initialize repository

--- democracy-orchestrator/src/main/java/com/democracy/democracy_orchestrator/application/services/TokenServiceImpl.java
+++ democracy-orchestrator/src/main/java/com/democracy/democracy_orchestrator/application/services/TokenServiceImpl.java
@@ -3,6 +3,8 @@ package com.democracy.democracy_orchestrator.application.services;
 
 
 import com.democracy.democracy_orchestrator.domain.models.KeyCloakToken;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
@@ -14,21 +16,38 @@ import java.util.Objects;
 @Service
 public class TokenServiceImpl implements TokenService{
 
+    //@Value("${client-id}")
+    private final String clientId = "democracy_client";
 
-    @Override
+    //@Value("${client-secret}")
+    private final String clientSecret = "YYcdVQO1lB9F5IjxjN6ljHueBWhZz1aZ";
+
+    //@Value("${gran-type}")
+    private final String granType = "password";
+
+    //@Value("${user-name}")
+    private final String userName ="raraherher9274";
+
+    //@Value("${password}")
+    private final String password ="raraherher9274";
+
+    //@Value("${url}")
+    private final String url = "http://localhost:8181/realms/democracy_realm/protocol/openid-connect/token";
+
+    //@Override
     public String obtainToken(){
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("client_id","democracy_client");
-        map.add("client_secret","YYcdVQO1lB9F5IjxjN6ljHueBWhZz1aZ");
-        map.add("grant_type","password");
-        map.add("username","raraherher9274");
-        map.add("password","raraherher9274");
+        map.add("client_id",clientId);
+        map.add("client_secret",clientSecret);
+        map.add("grant_type",granType);
+        map.add("username",userName);
+        map.add("password",password);
         HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(map, headers);
         ResponseEntity<KeyCloakToken> response =
-                restTemplate.exchange("http://localhost:8181/realms/democracy_realm/protocol/openid-connect/token",
+                restTemplate.exchange(url,
                         HttpMethod.POST,
                         entity,
                         KeyCloakToken.class);
